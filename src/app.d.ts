0611df30975571fddee0a90986f274bea9ee6020
Jonathan Bangert <jonathan@bangert.dk>
2024-06-14 22:56:41 +0200

    Setup for cloudflare more?

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,12 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+        interface Platform {
+            env: Env
+            cf: CfProperties
+            ctx: ExecutionContext
+        }
+    }
 }
 
 export {};
