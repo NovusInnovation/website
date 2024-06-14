8c8722077ceaee99330ebb7a991c4de3e61cec0a
Jonathan Bangert <jonathan@bangert.dk>
2024-06-14 21:45:29 +0200

    chore: Update Svelte adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -11,19 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
+		adapter: adapter()
 	}
 };
 
