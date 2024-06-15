ea4818e61613859151e09120e24cf571258fc4c6
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 15:44:51 +0200

    Idk margin top logo

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -7,9 +7,9 @@
 
     <nav class="w-screen sticky top-0 px-4">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <div class="flex ">
+            <div>
                 <a href="/">
-                    <img src="novusfull.png" alt="Logo" class="w-30 h-14" />
+                    <img src="novusfull.png" alt="Logo" class="w-30 h-14 mt-1" />
                 </a>
             </div>
             <ul class="flex flex-row mt-4 gap-3 items-center">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 <title>Novus Group</title>
 <meta name="description" content="Novus Group is a software development company that specializes in web development, mobile development, and custom software solutions.">
 
-<div class="container w-full items-center flex px-auto justify-center pt-12">
+<div class="container w-screen items-center flex px-auto justify-center pt-12">
     {#if showLogo}
         <img src="/novusfull.png" alt="NovusGroup Logo" width="400" height="400" in:fly={{
             y: 100,
