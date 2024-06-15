8ffab0f2722b42187a34c3e25969e16b2f27dee1
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2024-06-15 08:54:36 -0400

    style

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -2,12 +2,13 @@
     import * as m from '$lib/paraglide/messages';
 </script>
 
-<header class="bg-gray-200">
+<header class="bg-gray-200" style="min-height: 7vh;">
+
     <nav class="w-screen sticky top-0 px-4">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <div class="flex ">
                 <a href="/">
-                    <img src="/favicon.png" alt="Logo" class="w-14 h-14" />
+                    <img src="novusfull.png" alt="Logo" class="w-30 h-14" />
                 </a>
             </div>
             <ul class="flex flex-row mt-4 gap-3">
