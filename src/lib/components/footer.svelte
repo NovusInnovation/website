028b0bd15414e46725647d8249c30e00f096f6bb
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 08:50:25 +0200

    Add tailwind

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let year = new Date().getFullYear();
+</script>
+
+<footer class="bg-gray-200 py-4 absolute bottom-0 w-screen">
+    <div class="mx-auto text-center">
+        <p class="text-gray-600">© {year} Novus Group</p>
+    </div>
+</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Footer from "$lib/components/footer.svelte";
+    import '../app.css';
+</script>
+
+<main class="min-h-screen">
+    <slot></slot>
+    <Footer />
+</main>
