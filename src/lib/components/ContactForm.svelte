dfcffce518b3acb318d987f7c0b496468ae079e9
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 19:20:48 +0200

    Translate contact form

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -14,16 +14,16 @@
 		<h1 class="font-bold text-4xl">{m.contact_us()}</h1>
 		<p class="">{m.about_us_expanded()}</p>
 		<div class="flex flex-row gap-4">
-			<Input type="text" name="frist_name" placeholder="Fornavn" />
-			<Input type="text" name="last_name" placeholder="Efternavn" />
+			<Input type="text" name="frist_name" placeholder={m.first_name()} />
+			<Input type="text" name="last_name" placeholder="{m.last_name()}" />
 		</div>
-		<Input type="email" name="email" placeholder="Email" />
-		<Textarea name="message" placeholder="Besked" class="min-h-20 flex-1 resize-none" />
+		<Input type="email" name="email" placeholder="{m.email()}" />
+		<Textarea name="message" placeholder="{m.message()}" class="min-h-20 flex-1 resize-none" />
 		<Input type="text" name="honeypot" style="display:none" />
-		<input type="hidden" name="accessKey" value=" cf1c27fe-431b-487e-8ac9-1f0ff15f1d86" />
-		<input type="hidden" name="subject" value="Contact us from - akademia.cc" />
+		<input type="hidden" name="accessKey" value="cf1c27fe-431b-487e-8ac9-1f0ff15f1d86" />
+		<input type="hidden" name="subject" value="Contact us from - novusgroup" />
 		<input type="hidden" name="replyTo" value="@" />
-		<input type="hidden" name="redirectTo" value="https://akademia.cc/" />
+		<input type="hidden" name="redirectTo" value="https://novusgroup.dk/confirmed" />
 		<Button type="submit" class="text-lg w-48 h-12">{m.contact_send_message()}</Button>
 	</form>
 </div>
---
+++ src/routes/confirmed/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { m } from "$lib";
+	import { onMount } from "svelte";
+
+    onMount(() => {
+        goto("/");
+    });
+</script>
+
+<div>
+    <h1>{m.message_confirm_title()}</h1>
+    <p>{m.message_confirm()}</p>
+</div>
