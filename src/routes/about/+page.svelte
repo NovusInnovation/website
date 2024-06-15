dd6359b973d60e2d8bbfbe107bba4960ffe1f6ff
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2024-06-15 08:44:13 -0400

    more yap

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,7 @@
-Welcome to Novus Group. We are a collective of developers dedicated to shaping the technology of the future. We have developed several technical solutions and won various awards. Together, we build complete end-to-end solutions. Our solutions range from web development to mobile applications and video games.
+<body class="flex items-center justify-center  text-center">
+    <div>
+        <h1 class="text-3xl font-bold">Welcome to Novus Group</h1>
+        <p class="mt-4 text-lg">We are a collective of 4 developers dedicated to shaping the technology of the future. We have developed several technical solutions and won various awards. Together, we build complete end-to-end solutions. Our solutions range from web development to mobile applications and video games.</p>
+    </div>
+
+</body>
