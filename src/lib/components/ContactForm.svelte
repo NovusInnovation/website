f18e50f31699fd1c772c1cd5679245cd2a2e2db0
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 17:29:52 +0200

    Add contact form

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -2,6 +2,7 @@
 	import Input from './ui/input/input.svelte';
 	import { Textarea } from './ui/textarea';
 	import Button from './ui/button/button.svelte';
+	import { m } from '$lib';
 </script>
 
 <div class="p-10">
@@ -10,11 +11,8 @@
 		method="post"
 		class="flex h-full flex-col gap-4 lg:max-w-[20rem] xl:max-w-[30rem] 2xl:max-w-[40rem]"
 	>
-		<h1 class="font-bold text-4xl">Kontakt os</h1>
-		<p class="">
-			Har du spørgsmål, ris eller ros? Så tøv ikke med at kontakte os! Vi vil gøre vores bedste for
-			at besvare din henvendelse hurtigst muligt.
-		</p>
+		<h1 class="font-bold text-4xl">{m.contact_us()}</h1>
+		<p class="">{m.about_us_expanded()}</p>
 		<div class="flex flex-row gap-4">
 			<Input type="text" name="frist_name" placeholder="Fornavn" />
 			<Input type="text" name="last_name" placeholder="Efternavn" />
@@ -26,6 +24,6 @@
 		<input type="hidden" name="subject" value="Contact us from - akademia.cc" />
 		<input type="hidden" name="replyTo" value="@" />
 		<input type="hidden" name="redirectTo" value="https://akademia.cc/" />
-		<Button type="submit" class="text-lg w-48 h-12">Send besked</Button>
+		<Button type="submit" class="text-lg w-48 h-12">{m.contact_send_message()}</Button>
 	</form>
 </div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,7 @@
-Email: hey@novusgroup.dk
+<script>
+	import ContactForm from "$lib/components/ContactForm.svelte";
+
+</script>
+Email: contact@novusgroup.dk
+
+<ContactForm />
