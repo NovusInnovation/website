d7cb09a910b2ab627e1d8e90eb523bd272f2c6cc
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-15 20:34:06 +0200

    Add border rounding to footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,7 +4,7 @@
 	export let year = new Date().getFullYear();
 </script>
 
-<footer class="bg-card">
+<footer class="bg-card rounded-t-2xl">
 	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 		<div class="sm:flex sm:items-center sm:justify-between">
 			<a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
