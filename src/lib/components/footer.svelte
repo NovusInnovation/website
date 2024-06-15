bf7de4b4348f7312aa81699bbe8aa0a9b1ec6fe9
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-06-15 17:17:44 +0200

    fix width overflowing and use shadcn colors

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,32 +1,38 @@
 <script>
-	import { m } from "$lib";
+	import { m } from '$lib';
 
-    export let year = new Date().getFullYear();
+	export let year = new Date().getFullYear();
 </script>
 
-<footer class="bg-white dark:bg-gray-900 rounded-t-xl">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/favicon.png" class="h-8" alt="Novus Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Novus</span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="/home" class="hover:underline me-4 md:me-6">{ m.navbar_home() }</a>
-                </li>
-                <li>
-                    <a href="/about" class="hover:underline me-4 md:me-6">{ m.navbar_about() }</a>
-                </li>
-                <li>
-                    <a href="/projects" class="hover:underline me-4 md:me-6">{ m.navbar_projects() }</a>
-                </li>
-                <li>
-                    <a href="/contact" class="hover:underline">{ m.navbar_contact() }</a>
-                </li>
-            </ul>
-        </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {year} <a href="/" class="hover:underline">Novus Group™</a>. All Rights Reserved.</span>
-    </div>
+<footer class="bg-card">
+	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+				<img src="/favicon.png" class="h-8" alt="Novus Logo" />
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+					>Novus</span
+				>
+			</a>
+			<ul
+				class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
+			>
+				<li>
+					<a href="/home" class="hover:underline me-4 md:me-6">{m.navbar_home()}</a>
+				</li>
+				<li>
+					<a href="/about" class="hover:underline me-4 md:me-6">{m.navbar_about()}</a>
+				</li>
+				<li>
+					<a href="/projects" class="hover:underline me-4 md:me-6">{m.navbar_projects()}</a>
+				</li>
+				<li>
+					<a href="/contact" class="hover:underline">{m.navbar_contact()}</a>
+				</li>
+			</ul>
+		</div>
+		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+		<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
+			>© {year} <a href="/" class="hover:underline">Novus Group™</a>. All Rights Reserved.</span
+		>
+	</div>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
-    import { fly } from 'svelte/transition';
-    import { backOut } from 'svelte/easing';
-    import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { backOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
 
-    let showLogo = false;
+	let showLogo = false;
 
-    onMount(() => {
-        setTimeout(() => {
-            showLogo = true;
-        }, 250);
-    });
+	onMount(() => {
+		setTimeout(() => {
+			showLogo = true;
+		}, 250);
+	});
 </script>
 
 <title>Novus Group</title>
-<meta name="description" content="Novus Group is a software development company that specializes in web development, mobile development, and custom software solutions.">
+<meta
+	name="description"
+	content="Novus Group is a software development company that specializes in web development, mobile development, and custom software solutions."
+/>
 
-<div class="container w-screen items-center flex px-auto justify-center pt-12">
-    {#if showLogo}
-        <img src="/novusfull.png" alt="NovusGroup Logo" width="400" height="400" in:fly={{
-            y: 100,
-            easing: backOut
-        }}>
-    {/if}
+<div class="container items-center flex px-auto justify-center pt-12">
+	{#if showLogo}
+		<img
+			src="/novusfull.png"
+			alt="NovusGroup Logo"
+			width="400"
+			height="400"
+			in:fly={{
+				y: 100,
+				easing: backOut
+			}}
+		/>
+	{/if}
 </div>
