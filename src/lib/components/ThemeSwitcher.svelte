619c167b02bede7061b2e14985e43bee56383af0
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-15 21:10:59 +0200

    link color

--- src/lib/components/ThemeSwitcher.svelte
+++ src/lib/components/ThemeSwitcher.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
-    import { Sun } from "lucide-svelte";
-    import { Moon } from "lucide-svelte";
-    import { toggleMode } from "mode-watcher";
-    import { Button } from "$lib/components/ui/button/index.js";
-  </script>
-   
-  <Button on:click={toggleMode} variant="link" size="icon" class="hover:text-gray-500 -mr-5">
-    <Sun
-      class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <Moon
-      class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </Button>
+	import { Sun } from 'lucide-svelte';
+	import { Moon } from 'lucide-svelte';
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Button on:click={toggleMode} variant="link" size="icon" class="-mr-5">
+	<Sun class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+	<Moon
+		class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
