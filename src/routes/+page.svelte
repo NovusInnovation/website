181123a1ed0458d04cd2cf2b239cdabeac88ac0d
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 11:29:17 +0200

    Animate logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to NovusGroup</h1>
+<script lang="ts">
+    import { fly } from 'svelte/transition';
+    import { backOut } from 'svelte/easing';
+    import { onMount } from 'svelte';
 
+    let showLogo = false;
+
+    onMount(() => {
+        setTimeout(() => {
+            showLogo = true;
+        }, 250);
+    });
+</script>
+
+<div class="container w-screen items-center flex px-auto justify-center pt-12">
+    {#if showLogo}
+        <img src="/novusfull.png" alt="NovusGroup Logo" width="400" height="400" in:fly={{
+            y: 100,
+            easing: backOut
+        }}>
+    {/if}
+</div>
