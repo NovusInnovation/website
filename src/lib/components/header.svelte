5a7e4d6c0c8d2446d37da1b315e0d08ccd8088b0
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 11:13:01 +0200

    Add header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,18 @@
+
+<header>
+    <nav class="w-screen sticky top-0 px-4">
+        <div class="flex flex-wrap items-center justify-between mt-auto max-w-screen-xl">
+            <div class="flex ">
+                <a href="/">
+                    <img src="/favicon.png" alt="Logo" class="w-16 h-16" />
+                </a>
+            </div>
+            <ul class="flex flex-row mt-4 gap-3">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">Om os</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/contact">Kontakt</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Footer from "$lib/components/footer.svelte";
+	import Header from "$lib/components/header.svelte";
     import '../app.css';
 </script>
 
 <main class="min-h-screen">
+    <Header />
     <slot></slot>
     <Footer />
 </main>
