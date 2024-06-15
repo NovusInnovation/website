177c7904da0b737d11c2dc8d517fdb6e6b30bd0f
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 15:46:18 +0200

    Fix layout

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,6 @@
 <div class="flex items-center justify-center text-center">
-    <h1 class="text-3xl font-bold">Welcome to Novus Group</h1>
-    <p class="mt-4 text-lg">We are a collective of 4 developers dedicated to shaping the technology of the future. We have developed several technical solutions and won various awards. Together, we build complete end-to-end solutions. Our solutions range from web development to mobile applications and video games.</p>
+    <div>
+        <h1 class="text-3xl font-bold">Welcome to Novus Group</h1>
+        <p class="mt-4 text-lg">We are a collective of 4 developers dedicated to shaping the technology of the future. We have developed several technical solutions and won various awards. Together, we build complete end-to-end solutions. Our solutions range from web development to mobile applications and video games.</p>
+    </div>
 </div>
