bab3f17602f8f475d6c7367616a80a2c6d5d978b
Jonathan Bangert <jonathan@bangert.dk>
2024-06-15 14:08:52 +0200

    Aliases

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),sveltekit()]
+	plugins: [
+		paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),
+		sveltekit()
+	]
 });
