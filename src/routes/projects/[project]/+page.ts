04052f738974325116c3e6611b58a35d7ba89268
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-21 17:02:00 +0200

    Use plugin-dynamic-import-vars to make dynamic imports possible

--- src/routes/projects/[project]/+page.ts
+++ src/routes/projects/[project]/+page.ts
@@ -2,20 +2,20 @@ import { error } from '@sveltejs/kit';
 import { languageTag } from '$lib/paraglide/runtime.js';
 
 export async function load({ params, depends }) {
-    const project = params.project;
-    depends("paraglide:lang");
+	const project = params.project;
+	depends('paraglide:lang');
 
-    try {
-        const lang = languageTag();
-        const data = (await import('../../../lib/projects/' + project + "_" + lang + '.md'));
-        const metadata = data.metadata;
+	try {
+		const lang = languageTag();
+		const data = await import(`../../../lib/projects/${project}_${lang}.md`);
+		const metadata = data.metadata;
 
-        return {
-            content: data.default,
-            title: metadata.title
-        }
-    } catch (e) {
-        console.error(e);
-        return error(404, 'Not found');
-    }
+		return {
+			content: data.default,
+			title: metadata.title
+		};
+	} catch (e) {
+		console.error(e);
+		return error(404, 'Not found');
+	}
 }
