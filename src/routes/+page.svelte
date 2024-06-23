fe096b29bdff984a440f89c683591653af210752
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-23 20:00:39 +0200

    Add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,17 +18,13 @@
 	content="Novus Group is a software development company that specializes in web development, mobile development, and custom software solutions."
 />
 
-<div class="container items-center flex px-auto justify-center pt-12">
-	{#if showLogo}
-		<img
-			src="/novusfull.png"
-			alt="NovusGroup Logo"
-			width="400"
-			height="400"
-			in:fly={{
-				y: 100,
-				easing: backOut
-			}}
-		/>
-	{/if}
+<div class="items-center flex px-auto justify-center pt-12">
+	<h1
+		class="max-w-[30rem] md:max-w-full md:text-red-500 md:text-5xl md:whitespace-nowrap lg:text-6xl text-4x sm:text-5xl align-middle text-center"
+	>
+		<span>The team that’s Ready</span>
+		<br class="hidden md:block" />
+		to build <span class="italic">your</span> next
+		<span> Website </span>
+	</h1>
 </div>
