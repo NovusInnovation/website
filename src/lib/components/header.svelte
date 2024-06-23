662cf6081ea88cd36857094775f621c0fa03c962
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-06-23 17:25:16 +0200

    add svg

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,7 +6,10 @@
 
 <header class="" style="min-height: 7vh;">
 	<nav class="w-screen sticky top-0 px-4">
-		<div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+		<div
+			class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"
+			data-sveltekit-preload-data
+		>
 			<div>
 				<a href="/">
 					<img src="/novusfull.png" alt="Logo" class="w-30 h-14 mt-1" />
@@ -15,7 +18,7 @@
 			<ul class="flex flex-row mt-4 gap-5 items-center">
 				<li><a href="/">{m.navbar_home()}</a></li>
 				<li><a href="/about">{m.navbar_about()}</a></li>
-				<!-- <li><a href="/projects">{m.navbar_projects()}</a></li> -->
+				<li><a href="/projects">{m.navbar_projects()}</a></li>
 				<li><a href="/contact">{m.navbar_contact()}</a></li>
 				<ThemeSwitcher />
 				<LanguageSwitcher />
